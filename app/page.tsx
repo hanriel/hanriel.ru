12643497eeb724b2290e7bd6115719ac1e9c8576
Fedoseev Danil <hanriel59@gmail.com>
2026-03-06 18:23:36 +0500

    fixCalendar

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,9 @@ export default function Home() {
         <iframe src="https://calendar.google.com/calendar/embed?height=740&wkst=2&ctz=Asia%2FYekaterinburg&showPrint=0&showTz=0&showCalendars=0&mode=WEEK&showTitle=0&showTabs=0&src=aGFucmllbDU5QGdtYWlsLmNvbQ&src=YTRmZGRlYmU4NGZlMGMzY2I5M2VjOTYwZmVhODBkZDg1MTEzZWNhOTk0ODQwYzA5NDcyYzkzOWY3ZGQxMGNjMUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=Njg5MDA0NDY0ZjkwZjk1Njk5YjE2YTNiNDNmOTExMmY3MjU0NGNkZDdiNGY4MzU4YTFiZTdhM2MwZjZlMzVhMUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=cnUucnVzc2lhbiNob2xpZGF5QGdyb3VwLnYuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%230088ff&color=%237cb342&color=%23ff8d28&color=%230b8043"
           width={"100%"}
           height={"740px"}></iframe>
+        <iframe
+          width={"100%"}
+          height={"740px"} src="https://cloud.pmkspo.ru/index.php/apps/calendar/embed/dRYDfQYB9rgMAQHj"></iframe>
       </main>
   );
 }
