48d22e8bdcb13a11588f81304cbbd725ed265e50
Fedoseev Danil <hanriel59@gmail.com>
2026-05-06 22:38:19 +0500

    update portfolio

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,14 +1,7 @@
 export default function Home() {
   return (
-      <main className="flex min-h-screen w-full max-w-7xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <main className="flex min-h-screen w-full max-w-7xl flex-col items-center justify-normal py-32 px-16 bg-white dark:bg-black sm:items-start">
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            Упс...сайт пока в режиме глобального обслуживания...
-          </h1>
-          <ul>
-              <li><a href="https://pmk.hanriel.ru/">UGS PORTAL</a></li>
-              <li><a href="https://vkr.hanriel.ru">VKR Gibadullin</a></li>
-          </ul>
         </div>
         <iframe
           width={"100%"}
--- apps/web/app/portfolio/page.tsx
+++ apps/web/app/portfolio/page.tsx
@@ -1,28 +1,35 @@
+import { CardWrapper } from "@/components/card";
 import Card from "@/components/card"
 
 export default function Portfolio() {
   return (
-    <main className="flex min-h-screen w-full max-w-7xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <main className="flex min-h-screen w-full max-w-7xl flex-col items-center justify-normal py-32 px-16 bg-white dark:bg-black sm:items-start">
 
       <h1 className="croll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">Портфолио</h1>
       
-      <div className="w-full flex gap-8">
-        <Card
-          date="26.02.2026"
-          text="КП UGS PORTAL"
-          descr="Интернет портал для учета и автомаизации работы с успеваемостью студентов"
-        ></Card>
+      <div className="w-full flex gap-8 pt-8">
+        <CardWrapper href={'https://pmk.hanriel.ru/'}>
+          <Card
+            date="26.02.2026"
+            text='Курсовой проект по задам данных "UGS PORTAL"'
+            descr="Информационная система для учета и автомаизации работы с успеваемостью студентов"
+          >
+          </Card>
+        </CardWrapper>
+        <CardWrapper href={'https://vkr.hanriel.ru/'}>
         <Card
           date="25.02.2026"
-          text="ВКР Gibadullin"
+          text="Выпускная квалификационная работа"
           descr="Генератор расписания ПМК"
         ></Card>
-        <Card
-          date="24.02.2026"
-          text="КП UGS PORTAL"
-          descr="Интернет портал для учета и автомаизации работы с успеваемостью студентов"
-        ></Card>
-
+        </CardWrapper>
+        <CardWrapper href={'https://ugs.p87.pmkspo.ru/'}>
+          <Card
+            date="24.02.2026"
+            text='Курсовой проект Портал УГС 09.00.00'
+            descr="Информационная система для учета и автомаизации работы с успеваемостью студентов"
+          ></Card>
+        </CardWrapper>
       </div>
     </main>
   );
